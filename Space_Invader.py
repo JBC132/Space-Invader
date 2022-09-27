14474c46d412d62f9251269663c2837130291aac
이진영 <jakerhee321@gmail.com>
2022-09-27 12:29:44 +0900

    Window

--- Space_Invader.py
+++ Space_Invader.py
@@ -3,6 +3,11 @@ import os
 import random
 import time
 
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+
+
 # Enemy
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
@@ -19,3 +24,19 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # Background
 BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        # When press X button
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+main()
+
