2cd33a6ca4b1e5240c57a70a2d3674fdf8145834
이진영 <jakerhee321@gmail.com>
2022-09-29 12:30:46 +0900

    Ships and Movements

--- Space_Invader.py
+++ Space_Invader.py
@@ -26,6 +26,20 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y, 50,50))
+
+
 def main():
     run = True
     FPS = 60
@@ -33,6 +47,8 @@ def main():
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
     clock = pygame.time.Clock()
+    player_vel = 5
+    ship = Ship(300,650)
 
     def redraw_window():
         WIN.blit(BG, (0,0))
@@ -42,6 +58,8 @@ def main():
 
         WIN.blit(lives_label, (10,10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10,10))
+
+        ship.draw(WIN)
 
         pygame.display.update()
 
@@ -53,6 +71,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        # Movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            ship.x -= player_vel
+        if keys[pygame.K_d]:
+            ship.x += player_vel
+        if keys[pygame.K_w]:
+            ship.y -= player_vel
+        if keys[pygame.K_s]:
+            ship.y += player_vel
+        
+        
+
+
 
 main()
 
