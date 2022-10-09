a73934638488779cf1ad3aa9ccecb357ebfa0bfb
이진영 <jakerhee321@gmail.com>
2022-10-09 20:30:21 +0900

    Collision

--- Space_Invader.py
+++ Space_Invader.py
@@ -88,7 +88,9 @@ class Enemy(Ship):
         self.y += vel
         
 def collide(obj1, obj2):
-    pass
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2, (offset_x, offset_y)) != None
 
 def main():
     run = True
